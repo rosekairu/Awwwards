87be54397c876cd5efcf6a2707d0f37c747bcc02
Rose Kairu <rosekairu@gmail.com>
2020-07-12 22:19:57 +0300

    configured app urls

--- sites/urls.py
+++ sites/urls.py
@@ -6,14 +6,19 @@ from . import views
 
 
 urlpatterns=[
-    path('', views.home_page,name = 'home_page'),
+    path('', views.home_page,name = 'home'),
     path('search/', views.search_results, name='search_results'),
-    path('profile/(?P<username>\w+)', views.profile, name='profile'),
+    #url(r'^profile/(?P<username>[\w.@+-]+)/$', views.profile, name='profile'),
+    path('project/<post>/', views.view_project, name='project'),
+    path('profile/<username>/', views.profile, name='profile'),
     path('upload/', views.upload_project, name='upload_project'),
-    path('edit', views.edit, name='edit_profile'),
-    path('rate/(?P<project_id>\d+)',views.rate_project, name='rate'),
-    path('vote/(?P<project_id>\d+)',views.vote, name='vote'),
+    path('edit', views.edit, name='edit-profile'),
+    #path('rate/',views.rate_project, name='rate'),
+    url(r'^rate/(?P<project_id>\d+)',views.rate_project, name='rate'),
+    url(r'^vote/(?P<project_id>\d+)',views.vote, name='vote'),
+    #path('vote/',views.vote, name='vote'),
     path('api/profile/', views.ProfileList.as_view())
+    
 ]
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
