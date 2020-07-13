1621b619b7e401e51da301afdf4d9ba5232956db
Rose Kairu <rosekairu@gmail.com>
2020-07-13 04:12:22 +0300

    made migrations

--- sites/admin.py
+++ sites/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Project, Rate, Comment
+from .models import Profile, Project, Rate
 
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Project)
 admin.site.register(Rate)
-admin.site.register(Comment)
 
 
--- sites/apps.py
+++ sites/apps.py
@@ -4,5 +4,4 @@ from django.apps import AppConfig
 class SitesConfig(AppConfig):
     name = 'sites'
 
-def ready(self):
-    import sites.signals
+
