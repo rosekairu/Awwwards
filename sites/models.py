fe7380ca9d602c1ff3fd055c0c18a6e40e73c430
Rose Kairu <rosekairu@gmail.com>
2020-07-13 04:11:11 +0300

    modified models

--- sites/models.py
+++ sites/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
 import datetime as dt
+
+
+
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -9,9 +12,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User,default="",on_delete=models.CASCADE, primary_key=True)    
     bio = models.TextField(default="", max_length = 50,null = True)
     contact_info = models.CharField(max_length=200, blank=True)
-    join_date = models.DateTimeField(auto_now_add=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True, null=True)
     profile_Id = models.IntegerField(default=0)
-    all_projects = models.ForeignKey('Project', on_delete=models.PROTECT, null=True)
     rate = models.ManyToManyField('Project', related_name='image',max_length=30)
 
     def __str__(self):
@@ -49,10 +51,10 @@ class Profile(models.Model):
     class Meta:
         db_table = 'profiles'
 
-
+    
 class Project(models.Model):
     screenshot = models.ImageField(upload_to = 'images/')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     project_name = models.CharField(max_length =10)
     pub_date = models.DateTimeField(auto_now_add=True, null=True)
     details = models.TextField()
@@ -62,6 +64,7 @@ class Project(models.Model):
     usability = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
     creativity = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
     content = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    average = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
     vote_submissions = models.IntegerField(default=0)
 
 
@@ -69,6 +72,9 @@ class Project(models.Model):
         ordering = ['-pk']
 
     def save_project(self):
+        self.save()
+
+    def delete_project(self):
         self.save()
     
     @classmethod
@@ -83,8 +89,8 @@ class Project(models.Model):
 
     @classmethod
     def search_by_profile(cls,search_term):
-        proj = cls.objects.filter(profile__name__icontains=search_term)
-        return proj
+        app = cls.objects.filter(profile__name__icontains=search_term)
+        return app
 
     @classmethod
     def get_profile_projects(cls, profile):
@@ -97,12 +103,12 @@ class Project(models.Model):
         return identity
 
 class Rate(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT, null = True)
-    project = models.ForeignKey(Project,on_delete=models.PROTECT,related_name='rate',null=True)
-    design = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
-    usability = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)], null=True)
-    creativity = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],blank=True,null=True)
-    content = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE,related_name='rate',null=True)
+    design = models.CharField(max_length=30)
+    usability = models.CharField(max_length=8, default=0)
+    content = models.CharField(max_length=8,blank=True,null=True)
+    creativity = models.CharField(max_length=8,blank=True,null=True)
     average = models.FloatField(max_length=8)
     
 
@@ -116,7 +122,7 @@ class Rate(models.Model):
         self.delete()
 
     class Meta:
-        db_table = 'ratings'
+        ordering = ['-id']
 
     @classmethod
     def get_rate(cls, profile):
@@ -129,19 +135,3 @@ class Rate(models.Model):
         return rating
 
 
-class Comment(models.Model):
-    comment = models.CharField(max_length=80, null=True)
-    user = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.comment
-
-    def save_comment(self):
-        self.save()
-
-    def delete_comment(self):
-        self.delete()
-
-    class Meta:
-        db_table = 'comments'
-        ordering = ["-id"]
